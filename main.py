26913edd5a887c8e59c4892f23c4901a880b9302
TokaMohamed-QC <toka.ezzmohamed@gmail.com>
2023-09-23 15:26:21 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+#variables
+
+_cars = 23
+cars= 52
+CARS = 20
+
+cars_number = 87
+
+print (cars)
+print (_cars)
+print (CARS)
+print (cars_number)
+
+string1 = "Hello"
+name = "Toka Mohamed"
+
+print (string1 +" "+ name)
+print ("{} {}".format(string1,name))
+ 
